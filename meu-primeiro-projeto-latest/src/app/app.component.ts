9577ccf8f7a1eab0583305fbe8e3c76f4b134fa7
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-14 17:57:28 -0300

    commit

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { NewComponent } from './components/new-component/new.component';
 import { CommonModule } from '@angular/common';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NewComponent, TemplateBindingComponent],
+  imports: [CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
   styles: ``,
   template: `
     <h2>Curso de Angular</h2>
     <app-template-binding />
+    <app-template-variables />
   `
 })
 export class AppComponent {
