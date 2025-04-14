8a542f48d6d512298a3a419e1e76a37927a93fbf
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-14 17:38:44 -0300

    Template Variables

---
+++ meu-primeiro-projeto-latest/src/app/components/template/template-variables/template-variables.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-variables',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-variables.component.html',
+  styleUrl: './template-variables.component.scss'
+})
+export class TemplateVariablesComponent {
+
+}
