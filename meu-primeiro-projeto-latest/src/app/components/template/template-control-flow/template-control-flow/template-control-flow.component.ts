520f4ffda47d435d6b689b6e2843879931e7b720
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-15 15:14:56 -0300

    commit

--- meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow/template-control-flow.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow/template-control-flow.component.ts
@@ -11,21 +11,5 @@ import { delay, Observable, of } from 'rxjs';
 })
 export class TemplateControlFlowComponent {
 
-  public isTrue = true;
-
-  public itens = [{name: 'Uver'}];
-  public loadingData$: Observable<string[]> = of([
-    'item 1',
-    'item 2',
-    'item 3',
-  ]).pipe(delay(3000));
-
-public trackByFn(index: number){
-  return index;
-}
-
-public addNewName(value: string){  
-  return this.itens.push({ name: value});
-}
-
+  public itens: Array<{name: string}> = [ {name: 'Uver'}];
 }
