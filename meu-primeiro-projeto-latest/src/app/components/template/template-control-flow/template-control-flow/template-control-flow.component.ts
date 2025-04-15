109247ae2933a5e05f2687f6094cbcd6edae9bea
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-15 11:30:19 -0300

    Control Flow e Diretiva estrutural

--- meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow/template-control-flow.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow/template-control-flow.component.ts
@@ -1,14 +1,26 @@
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
 
   public isTrue = true;
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
+
+public trackByFn(index: number){
+  return index;
+}
+
 }
