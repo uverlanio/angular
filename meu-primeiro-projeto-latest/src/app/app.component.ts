ade542f4af837e433206348879a92b528a95d8bf
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-15 15:40:40 -0300

    Deferrable Viewa

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,15 @@ import { TemplateControlFlowComponent } from './components/template/template-con
     NewComponent, 
     TemplateBindingComponent, 
     TemplateVariablesComponent, 
-    TemplateControlFlowComponent],
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewsComponent],
   styles: ``,
   template: `
     <h2>Curso de Angular</h2>
     <!--<app-template-binding />
-    <app-template-variables />-->
-    <app-template-control-flow />
+    <app-template-variables />
+    <app-template-control-flow />-->
+    <app-template-deferrable-views />
   `
 })
 export class AppComponent {
---
+++ meu-primeiro-projeto-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { NewComponent } from '../../new-component/new.component';
+
+@Component({
+  selector: 'app-template-deferrable-views',
+  standalone: true,
+  imports: [CommonModule, NewComponent],
+  templateUrl: './template-deferrable-views.component.html',
+  styleUrl: './template-deferrable-views.component.scss'
+})
+export class TemplateDeferrableViewsComponent {
+
+}
