821ea2fe0eeab8a3620a0ee2fb06a50fb68d52c9
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-23 14:01:05 -0300

    Criando o component Header

--- curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
