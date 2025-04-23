794485647c9c1df2774cf608e034d997ecbf5463
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-04-23 19:59:01 -0300

    Novo component Projects

---
+++ curso-angular-portfolio/src/app/modules/portfolio/interface/IProjects.interface.ts
@@ -0,0 +1,15 @@
+export interface IProjects{
+    
+    src: string,
+    alt: string,
+    title: string,
+    width: string,
+    height: string,
+    description: string
+    links: [
+        {
+            name: string,
+            href: string
+        }
+    ]       
+}
