de50e10679136ccf7300f1a51fad4eb44a90101e
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-02 16:33:37 -0300

    implementacao do angular material para uso do modal - projects e dialog components

--- curso-angular-portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+//Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
@@ -25,4 +30,11 @@ export class ProjectsComponent {
       ]
     }
   ])
+
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent,{
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
