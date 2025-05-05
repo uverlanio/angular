42910e388613f736080efaada34dc6915378e61d
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-05 17:49:48 -0300

    Uso do decorator Input

---
+++ curso-angular-portfolio/src/app/components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { InputComponent } from '../input/input.component';
+import { CommonModule } from '@angular/common';
+import { OutputComponent } from '../output/output.component';
+
+@Component({
+  selector: 'app-pai-ou-mae',
+  standalone: true,
+  imports: [CommonModule, InputComponent, OutputComponent],
+  templateUrl: './pai-ou-mae.component.html',
+  styleUrl: './pai-ou-mae.component.scss'
+})
+export class PaiOuMaeComponent {
+  public name = signal('Uverlanio Mauricio')
+}
