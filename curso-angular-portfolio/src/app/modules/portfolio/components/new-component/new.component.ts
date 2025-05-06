01d29ff0803475d1921712b60045dec71b223b7f
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-06 10:48:54 -0300

    contem arquivos das sessoes 1 a 6

---
+++ curso-angular-portfolio/src/app/modules/portfolio/components/new-component/new.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './new.component.html',
+  styleUrl: './new.component.scss'
+})
+export class NewComponent {
+
+  public name = 'New Component';
+
+}
