020fa0e0a19e1341838863bffa4fc0f247a5ca5d
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-07 10:47:40 -0300

    inclusao de botoes e scss

--- curso-angular-lista-de-tarefas/src/app/app.component.ts
+++ curso-angular-lista-de-tarefas/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `<p>Teste</p>
+  template: `
     <router-outlet />`
 })
 export class AppComponent {
--- curso-angular-lista-de-tarefas/src/app/app.routes.ts
+++ curso-angular-lista-de-tarefas/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './modules/to-do-list/pages/list/list.component';
 
-export const routes: Routes = [];
+// Pages
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    }
+];
