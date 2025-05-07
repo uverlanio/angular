71a182e726ac30f6add7cd6b12107ceeb45fe54b
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-07 15:27:08 -0300

    commit

--- curso-angular-lista-de-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
+++ curso-angular-lista-de-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IListItems.interface';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
   public addItem = signal(true);
+
+  #setListItems = signal<IListItems[]>([this.#parseItems()]);
+  getListItems = this.#setListItems.asReadonly();
+
+  #parseItems(){
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
+  public getInputAndAddItem(value: IListItems){
+    localStorage.setItem(
+      '@my-list', JSON.stringify([value])
+    );
+  }
 }
