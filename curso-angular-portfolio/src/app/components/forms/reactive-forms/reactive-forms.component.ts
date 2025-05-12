ae13862e1fd83cd97d14cf2572d45eef7008374f
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-12 14:43:03 -0300

    commit

---
+++ curso-angular-portfolio/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-forms',
+  imports: [],
+  templateUrl: './reactive-forms.component.html',
+  styleUrl: './reactive-forms.component.scss'
+})
+export class ReactiveFormsComponent {
+
+}
---
+++ curso-angular-portfolio/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-forms',
+  imports: [],
+  templateUrl: './template-driven-forms.component.html',
+  styleUrl: './template-driven-forms.component.scss'
+})
+export class TemplateDrivenFormsComponent {
+
+}
