61c37f38bd9a82fc95ad25de34920e73894ecd4d
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-12 14:59:08 -0300

    ssessao 14

--- curso-angular-portfolio/src/app/app.component.ts
+++ curso-angular-portfolio/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsComponent } from './components/forms/reactive-forms/reactive-forms.component';
+import { TemplateDrivenFormsComponent } from './components/forms/template-driven-forms/template-driven-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<router-outlet></router-outlet>',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ReactiveFormsComponent,
+    TemplateDrivenFormsComponent,
+  ],
+  template: ` <h1>Curso de Angular</h1>
+    <app-template-driven-forms />
+    <app-reactive-forms />
+    `,
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
