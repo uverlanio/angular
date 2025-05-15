0d616cb9db8530a27594790e1695cbe1b0747ba0
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-05-15 10:55:55 -0300

    fim da sessao 15

--- curso-angular-portfolio/src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ curso-angular-portfolio/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,7 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
+function textValidator(): ValidatorFn{
+  return (control: AbstractControl) => {
+    const hasUpperCase = /[A-Z]/.test(control.value)
+    const hasNumber = /[[0-9]/.test(control.value)
+    if(hasUpperCase && hasNumber){
+      return null;
+    }
+    return { textValidator: true };
+  }
+}
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
@@ -13,7 +23,7 @@ export class ReactiveFormsComponent {
   #fb = inject(FormBuilder);
 
   public profileForm = this.#fb.group({
-    name: [''],
+    name: ['', [Validators.required, textValidator()]],
     myStacks: this.#fb.group({
       front: ['Angular'],
       back: ['Java']
@@ -36,5 +46,11 @@ export class ReactiveFormsComponent {
     const addNewFood = this.#fb.control(newFood);
 
     myFavouriteFood.push(addNewFood);
+  }
+
+  public submit(){
+    console.log(this.profileForm.valid)
+    if(this.profileForm.valid)
+      console.log(this.profileForm.value)
   }
 }
