afedc6d6e4bde060930069663ccbcdae34ded7d5
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-06-16 07:00:37 -0300

    commit

--- curso-angular-portfolio/src/app/app.routes.ts
+++ curso-angular-portfolio/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { ServicosPrestadoComponent } from './pages/servicos-prestado/servicos-prestado.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 export const routes: Routes = [
@@ -18,5 +19,9 @@ export const routes: Routes = [
     {
         path: 'servicos/:id',
         component: ServicosPrestadoComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
